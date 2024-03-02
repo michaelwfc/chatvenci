142d1898b6c3f1e247eb42e311d8a55296f59e96
michaelwfc <wang_feicheng@163.com>
2024-03-02 15:49:32 +0800

    test utils in js

---
+++ spotify-web-playback/server/test.js
@@ -0,0 +1,5 @@
+const { getProxySetting, generateRandomString } = require('./utils');
+
+
+getProxySetting();
+generateRandomString(length = 10);
--- spotify-web-playback/server/utils.js
+++ spotify-web-playback/server/utils.js
@@ -1,4 +1,7 @@
-const { getProxyForUrl } = require('proxy-from-env');
+const { getProxyForUrl, proxyFromEnv } = require('proxy-from-env');
+const http = require('http');
+const https = require('https');
+
 
 
 function getProxySetting() {
@@ -12,4 +15,40 @@ function getProxySetting() {
     return proxyServer
 }
 
-getProxySetting()
+function setGlobalProxy(proxyServer = 'http://127.0.0.1:7890') {
+    // Get current proxy settings
+    // const currentProxy = process.env.http_proxy || process.env.HTTP_PROXY;
+    // console.log('Current proxy:', currentProxy);
+
+    // Set proxy settings
+    http.globalAgent = new http.Agent({ "proxy": proxyServer });
+    https.globalAgent = new https.Agent({ "proxy": proxyServer });
+
+    // Now all HTTP and HTTPS requests will use the specified proxy
+
+}
+
+// setGlobalProxy();
+// getProxySetting();
+
+
+// import generateRandomString from './utils';
+var generateRandomString = function (length) {
+    var text = '';
+    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+    for (var i = 0; i < length; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    console.log('text:', text);
+    return text;
+};
+
+
+// export default getProxySetting;
+module.exports = {
+    getProxySetting,
+    setGlobalProxy,
+    generateRandomString,
+
+};
